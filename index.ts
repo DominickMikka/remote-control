64580dd12843586216da81e97d36a3b9ea94ef0a
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-23 18:02:57 +0300

    feat: add draw_circle and draw_rectangle

--- index.ts
+++ index.ts
@@ -63,6 +63,68 @@ wss.on('connection', ws => {
     if (message.startsWith('mouse_position')) {
       const currentMousePos = robot.getMousePos();
       ws.send(`mouse_position ${currentMousePos.x}px,${currentMousePos.y}px`);
+    } else
+
+    if (message.startsWith('draw_circle')) {
+      ws.send(message);
+      const [command, radius] = message.toString().split(' ');
+      const currentMousePos = robot.getMousePos();
+
+      robot.setMouseDelay(1);
+
+      for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+        const x = currentMousePos.x + (+radius * Math.cos(i));
+        const y = currentMousePos.y + (+radius * Math.sin(i));
+        
+        robot.dragMouse(x, y);
+        robot.mouseToggle('down');
+      }
+      robot.mouseToggle('up');
+    } else
+
+    if (message.startsWith('draw_rectangle')) {
+      ws.send(message);
+      const [command, length, width] = message.toString().split(' ');
+      let currentMousePos = robot.getMousePos();
+
+      robot.setMouseDelay(1);
+      robot.mouseToggle('down');
+
+      let y = currentMousePos.y - +length;
+
+      while (currentMousePos.y !== y) {
+        robot.dragMouse(currentMousePos.x, currentMousePos.y);
+        currentMousePos.y--;
+      }
+
+      currentMousePos = robot.getMousePos();
+
+      let x = currentMousePos.x + +width;
+
+      while (currentMousePos.x !== x) {
+        robot.dragMouse(currentMousePos.x, currentMousePos.y);
+        currentMousePos.x++;
+      }
+
+      currentMousePos = robot.getMousePos();
+
+      y = currentMousePos.y + +length;
+
+      while (currentMousePos.y !== y) {
+        robot.dragMouse(currentMousePos.x, currentMousePos.y);
+        currentMousePos.y++;
+      }
+
+      currentMousePos = robot.getMousePos();
+
+      x = currentMousePos.x - +width;
+
+      while (currentMousePos.x !== x) {
+        robot.dragMouse(currentMousePos.x, currentMousePos.y);
+        currentMousePos.x--;
+      }
+
+      robot.mouseToggle('up');
     }
 
   });
